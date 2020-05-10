c545edd1432a58177f1f0ea8b9fe3c27e1913d2a
dadayada <nasushnyi@gmail.com>
2020-05-10 15:23:07 +0300

    Updates

---
+++ precache-manifest.377a027bae5eff5959f0160f1c31f670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd0f13fba1f56e0bae6f9b57394d404",
+    "url": "/pavlo-gleck/index.html"
+  },
+  {
+    "revision": "995adf1d3dd7bbfb84d9",
+    "url": "/pavlo-gleck/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "edefec93c53293e4ea13",
+    "url": "/pavlo-gleck/static/js/2.d293cec1.chunk.js"
+  },
+  {
+    "revision": "a13369e7405157f254f1ffcc4cc651e5",
+    "url": "/pavlo-gleck/static/js/2.d293cec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995adf1d3dd7bbfb84d9",
+    "url": "/pavlo-gleck/static/js/main.b8098e09.chunk.js"
+  },
+  {
+    "revision": "948471ccde73877a3257",
+    "url": "/pavlo-gleck/static/js/runtime-main.07ec3805.js"
+  }
+]);
